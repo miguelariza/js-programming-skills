1fb124ac87d03fd9c5c2acff46547d6d9b5e53e0
miguelariza <miguelangel.ariza@gmail.com>
2026-02-11 14:24:48 -0500

    Add prices to restaurante menu items in js file

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $69.99', 'Pasta - $32.99', 'Burger - $18', 'Salmon - $50'];
+const dessertMenu = ['Cake - $12.99', 'Ice Cream - $9.99', 'Pudding - $12', 'Fruit Salad - $25'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
