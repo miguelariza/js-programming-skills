55d4097d00efe511eb710368bc0d439f8d3bcb5e
miguelariza <miguelangel.ariza@gmail.com>
2026-02-12 13:25:12 -0500

    Adde delete book feature

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -29,7 +29,8 @@ function showBooks() {
         <p><strong>Author Name: </strong>${book.authorName}</p>
         <p><strong>Book Description: </strong>${book.bookDescription}</p>
         <p><strong>No. of Pages: </strong>${book.pagesNumber} page(s)</p>
-        <button onclick="editBook(${index})">Edit</button>`
+        <button onclick="editBook(${index})">Edit</button>
+        <button onclick="deleteBook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -49,4 +50,9 @@ function clearInputs() {
     document.getElementById('authorName').value = "";
     document.getElementById('bookDescription').value = "";
     document.getElementById('pagesNumber').value = "";
+}
+
+function deleteBook(index) {
+    books.splice(index, 1);
+    showBooks();
 }
