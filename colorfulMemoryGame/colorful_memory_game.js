bfc4a7112f21c9daa636337b746a7cda39317f5f
miguelariza <miguelangel.ariza@gmail.com>
2026-02-20 22:41:45 -0500

    Fix errors in timer function

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -21,7 +21,7 @@ function generateCards() {
 }
 
 function shuffle(arr) {
-    for (let i = arr.length - 1; i > 0; i++) {
+    for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j] = arr[j], arr[i]];
     }
@@ -59,7 +59,7 @@ function checkMatch() {
 
 function startGame() {
     let timeLeft = 30;
-    startbtn.disable = true;
+    startbtn.disabled = true;
     score = 0; // Reset score to zero
     scoreElement.textContent = `Score: ${score}`;
     startGameTimer(timeLeft);
