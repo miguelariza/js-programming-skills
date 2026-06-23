92ef8a52e49a68c86fcfbac62eb5a8221373afd1
miguelariza <miguelangel.ariza@gmail.com>
2026-06-23 19:32:51 -0400

    test weather report OK

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -2,27 +2,25 @@ function showweatherDetails(event) {
     event.preventDefault();
 
     const city = document.getElementById('city').value;
-    const apiKey = 'my_api_key';
-    //const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const apiKey = 'OPENWEATHER_API_KEY';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    //const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=${apiKey}&units=metric`;
-    
+
+    console.log(apiUrl);
+
     fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            //console.log(data);
-            const weatherInfo = document.getElementById('weatherInfo');
-            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
-                <p>Temperature: ${data.main.temp} &#8451</p>
-                <p>Weather: ${data.weather[0].description}</p>`;
-            })
-        .catch(error => {
-            console.error('Error fetching weather:', error);
-            const weatherInfo = document.getElementById('weatherInfo');
-            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
-        });
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+        <p>Temperature: ${data.main.temp} &#8451;</p>
+        <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+      });
 
 }
 
-document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
-
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
